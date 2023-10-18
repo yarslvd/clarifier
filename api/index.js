526999464b1a423baa7eaed2741823a0efb160a6
yarslvd <yaroslavdoroshenko0@gmail.com>
2023-10-18 10:07:37 +0300

    updated for host

--- api/index.js
+++ api/index.js
@@ -10,7 +10,7 @@ require('dotenv').config();
 
 app.use(adminBro.options.rootPath, routerAdmin);
 app.use(express.json());
-app.use(cors({origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({origin: 'https://clarifier.vercel.app', credentials: true }));
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(router);
--- client/src/redux/axios.js
+++ client/src/redux/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie'
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8000',
+    baseURL: 'https://clarifier-api.onrender.com',
     headers: {'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json'},
     credentials: 'include',
     withCredentials: true,
